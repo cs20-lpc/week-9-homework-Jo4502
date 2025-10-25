e9b4b084fc5600c431cb60c8df2110a232967cd6
Jo4502 <jcamador@zonemail.clpccd.edu>
2025-10-24 18:13:40 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,14 +9,14 @@ int main() {
 
     cout << "\nSimulation 1: Ticket Counter\n";
 
-    Queue<int>* ticketLine = QueueFactory::GetQueue<int>(10); // max 10 customers
+    Queue<int>* ticketLine = QueueFactory::GetQueue<int>(10); 
     const int SIM1_STEPS = 100;
 
     for (int i = 0; i < SIM1_STEPS; i++) {
-        int num = rand() % 3 - 1; // generates -1, 0, 1
+        int num = rand() % 3 - 1; 
         if (num < 0) {
             if (!ticketLine->isEmpty()) {
-                ticketLine->dequeue(); // one customer served
+                ticketLine->dequeue(); 
             }
         }
         else {
@@ -24,7 +24,7 @@ int main() {
                 ticketLine->enqueue(i);
             }
             catch (string& e) {
-                // queue full ? customer turned away
+                
             }
         }
     }
@@ -32,23 +32,21 @@ int main() {
     cout << "Remaining in line: " << ticketLine->getLength() << endl;
     delete ticketLine;
 
-    // -------------------------------------------------------------------------
-    // Simulation 2: Bank Line (LinkedQueue, unlimited size)
-    // -------------------------------------------------------------------------
+   
     cout << "\nSimulation 2: Bank Line\n";
 
-    Queue<int>* bankLine = QueueFactory::GetQueue<int>(); // linked list queue
+    Queue<int>* bankLine = QueueFactory::GetQueue<int>(); 
     const int SIM2_STEPS = 100;
 
     for (int i = 0; i < SIM2_STEPS; i++) {
-        int num = rand() % 4 - 1; // generates -1, 0, 1, 2
+        int num = rand() % 4 - 1; 
         if (num < 0) {
             if (!bankLine->isEmpty()) {
-                bankLine->dequeue(); // one customer leaves
+                bankLine->dequeue(); 
             }
         }
         else {
-            bankLine->enqueue(i); // new customer arrives
+            bankLine->enqueue(i); 
         }
     }
 
